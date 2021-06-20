1bbefef6c2146fa84409d70e1857a2a124dcc418
Kevin Schaefer <kevinss.hello@gmail.com>
2021-06-19 19:34:04 -0700

    pokemon list array added
    initial pokemon list array added to JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('hello world');
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    { name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
+    { name: 'Venusaur', height: 2, types: ['grass', 'poison']}
+  ];
