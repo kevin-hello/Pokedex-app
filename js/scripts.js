c4282ec72a94c9f7adf05ec2b830dae9b711c26c
Kevin Schaefer <kevinss.hello@gmail.com>
2021-06-22 23:28:50 -0700

    feat: JS document.write array with conditional
    -display Pokemon list and add additional text if a Pokemon item meets the specified condition.
    
    -tota11y script added

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ let pokemonList = [
     { name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
     { name: 'Venusaur', height: 2, types: ['grass', 'poison']}
   ];
+
+for (let i=0; i< pokemonList.length; i++){
+  if (pokemonList[i].height >1){
+  document.write(pokemonList[i].name) + document.write(' (height:') + document.write(pokemonList[i].height) + document.write(") - Wow that's big!<br>"); 
+  }
+  else
+  {
+    document.write(pokemonList[i].name) + document.write(' (height:') + document.write(pokemonList[i].height) + document.write(')<br>');
+  }}
