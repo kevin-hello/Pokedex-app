bcf3ce2b878cc57378cf03166e56adc9acfc34ae
Kevin Schaefer <kevinss.hello@gmail.com>
2021-06-29 03:30:03 -0700

    :refactor array wrapped in IIFE
    -pokemonList wrapped in an IIFE
    
    -tested add function by adding Pikachu data

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-
-let pokemonList = [
+let pokemonRepository = (function () {
+  let pokemonList = [
     {
       name: 'Bulbasaur',
       height: 0.7,
@@ -16,13 +16,35 @@ let pokemonList = [
       types: ['grass', 'poison']
     }
   ];
-pokemonList.forEach(function(pokemon){
-  document.write(pokemon.name + "(height:" + pokemon.height +")");
-  if (pokemon.height > 1) {
-    document.write(" - Wow that's big!")
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  function getAll(){
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+  
+})();
+
+pokemonRepository.add({
+  name:'Pikachu',
+  height: 1.5,
+  types: ['electric']
+})
+
+pokemonRepository.getAll().forEach((pokemon) => {
+  document.write(pokemon.name + "(height: " + pokemon.height + ")");
+  if (pokemon.height>1){
+    document.write(" -Wow that's big!")
   }
   document.write("<br>");
 });
+ 
 
 
 
@@ -41,24 +63,3 @@ pokemonList.forEach(function(pokemon){
 
 
 
-
-
-//let pokemonList = [
-//    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-//    { name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
-//    { name: 'Venusaur', height: 2, types: ['grass', 'poison']}
-//  ];
-//pokemonList.forEach(element => {
-  
-//});
-
-
-//  for (let i = 0; i < pokemonList.length; i++) {
-//    document.write(pokemonList[i].name + " (height:" + pokemonList[i].height + ")");
-//  
-//    if (pokemonList[i].height > 1) {
-//      document.write(" - Wow that's big!<br>");
-//    }
-//    document.write("<br>");
-//   
-//  }
