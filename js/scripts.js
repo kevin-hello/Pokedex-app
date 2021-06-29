099917286563bbc9f81471b17439b35070c42726
Kevin Schaefer <kevinss.hello@gmail.com>
2021-06-28 21:46:23 -0700

    :refactor pokemonList
    -for changed to forEach()

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,64 @@
-let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    { name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
-    { name: 'Venusaur', height: 2, types: ['grass', 'poison']}
-  ];
 
-  for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + " (height:" + pokemonList[i].height + ")");
-  
-    if (pokemonList[i].height > 1) {
-      document.write(" - Wow that's big!<br>");
+let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Ivysaur',
+      height: 1,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Venusaur',
+      height: 2,
+      types: ['grass', 'poison']
     }
-    document.write("<br>");
-   
+  ];
+pokemonList.forEach(function(pokemon){
+  document.write(pokemon.name + "(height:" + pokemon.height +")");
+  if (pokemon.height > 1) {
+    document.write(" - Wow that's big!")
   }
+  document.write("<br>");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let pokemonList = [
+//    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+//    { name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
+//    { name: 'Venusaur', height: 2, types: ['grass', 'poison']}
+//  ];
+//pokemonList.forEach(element => {
+  
+//});
+
+
+//  for (let i = 0; i < pokemonList.length; i++) {
+//    document.write(pokemonList[i].name + " (height:" + pokemonList[i].height + ")");
+//  
+//    if (pokemonList[i].height > 1) {
+//      document.write(" - Wow that's big!<br>");
+//    }
+//    document.write("<br>");
+//   
+//  }
