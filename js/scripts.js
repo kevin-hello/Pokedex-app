14960bf1ef9fa2eafaf440584ab0eaf570b67d8e
Kevin Schaefer <kevinss.hello@gmail.com>
2021-07-02 18:14:38 -0700

    :fix showDetails on click

--- js/scripts.js
+++ js/scripts.js
@@ -33,7 +33,10 @@ let pokemonRepository = (function () {
     let listItem = document.createElement("li");
     let button = document.createElement("button");
     button.innerText = pokemon.name;
-    button.addEventListener("click", showDetails(pokemon));
+    button.addEventListener("click", function(){
+      showDetails(pokemon)
+
+    });
     button.classList.add("button-class");
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
